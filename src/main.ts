bbebb0bdc05cc80af7d474e1b9a03b655b9c5c97
Rak Siv <rak.siva@nitric.io>
2022-03-28 15:43:23 +1000

    chore: add semver and error handling for 404s

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,18 @@ import * as tc from "@actions/tool-cache";
 import * as os from "os";
 import * as io from '@actions/io';
 import * as path from "path";
+import * as semver from "semver";
 
 
 function getNitricUrl(version, platform) { 
-    const baseUrl = `https://github.com/nitrictech/cli/releases/download/`
-    const url = `${baseUrl}v${version}/nitric_${version}`
+    const baseUrl = `https://github.com/nitrictech/cli/releases/download/`;
+    const url = `${baseUrl}v${version}/nitric_${version}`;
     if (platform == 'win32') {
-        return `${url}_Windows_x86_64.zip`
+        return `${url}_Windows_x86_64.zip`;
     } else if (platform == 'darwin') { 
-        return `${url}_macOS_x86_64.tar.gz`
+        return `${url}_macOS_x86_64.tar.gz`;
     } else { 
-        return `${url}_Linux_x86_64.tar.gz`
+        return `${url}_Linux_x86_64.tar.gz`;
     }        
 }
 
@@ -46,14 +47,24 @@ async function run() {
 
         const rp = os.platform();
         if (!(rp in platforms)) {
-          throw new Error("Unsupported operating system.");
+            throw new Error("Unsupported operating system.");
+        }
+
+        // Check version format
+        const version = core.getInput('version')?.trim();
+        if (!semver.valid(version)) { 
+            throw new Error("Incorrect version - Use semantic versioning E.g. 1.2.1");
         }
 
         // Download release version
-        const version = core.getInput('version')
-        const url = getNitricUrl(version, rp)
-        const downloaded = await tc.downloadTool(url);
-        core.info(`Downloaded package from - ${url}`);
+        const url = getNitricUrl(version, rp);
+        let downloaded
+        try { 
+            downloaded = await tc.downloadTool(url);
+            core.info(`Downloaded package from - ${url}`);
+        } catch (error) { 
+            throw new Error(`Could not download CLI from url : ${url}`);
+        }
 
         // Extract and add to path
         const destination = path.join(os.homedir(), ".nitric");
